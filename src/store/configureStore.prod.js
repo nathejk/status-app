0e61f50452ea913d185c84cd0d17278da20ea46f
Christoffer Bo Petersen <cb.petersen@gmail.com>
2016-09-15 22:19:56 +0200

    Fix typo in prod store

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -7,7 +7,7 @@ export default function configureStore(initialState, browserHistory) {
   const middewares = [
     // Add other middleware on this line...
 
-    routerMiddleware(browserHistory)
+    routerMiddleware(browserHistory),
 
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
