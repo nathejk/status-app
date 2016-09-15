a9e2e5fadeb5fbca75cdfca714b782b057e8d46e
Christoffer Bo Petersen <cb.petersen@gmail.com>
2016-09-15 21:50:33 +0200

    Set initial state

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 import { syncHistoryWithStore } from 'react-router-redux';
-import initialState from './reducers/initialState'
-const store = configureStore(initialState, browserHistory);
+
+const store = configureStore(undefined, browserHistory);
 
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
